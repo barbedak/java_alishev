4a2588c67dc4c6bc6266cf6c079643c67e600db5
Andrey Kurochkin <barbedak@gmail.com>
2022-04-12 11:30:14 +0300

    Dependency Injection (DI) witch XML, using external file with parameters. Using list properties in XML

--- app1/src/main/java/ru/kurochkin/springcourse/MusicPlayer.java
+++ app1/src/main/java/ru/kurochkin/springcourse/MusicPlayer.java
@@ -1,16 +1,19 @@
 package ru.kurochkin.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public MusicPlayer(){
+    public MusicPlayer() {
     }
 
     public String getName() {
@@ -29,11 +32,13 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList.addAll(musicList);
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
---
+++ app1/src/main/java/ru/kurochkin/springcourse/PopMusic.java
@@ -0,0 +1,8 @@
+package ru.kurochkin.springcourse;
+
+public class PopMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Makarena";
+    }
+}
