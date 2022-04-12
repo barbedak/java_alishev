13bca61a1bbd270649d31e038adace15f6afdabf
Andrey Kurochkin <barbedak@gmail.com>
2022-04-12 13:50:46 +0300

    Scope Prototype & Singleton. Init, Destroy & Factory methods

--- app1/src/main/java/ru/kurochkin/springcourse/ClassicalMusic.java
+++ app1/src/main/java/ru/kurochkin/springcourse/ClassicalMusic.java
@@ -1,6 +1,20 @@
 package ru.kurochkin.springcourse;
 
 public class ClassicalMusic implements Music {
+//  паттерн  Фабричный метод
+    private ClassicalMusic(){} //ограничение создания объекта через new, нужно использовать фабричный метод
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
