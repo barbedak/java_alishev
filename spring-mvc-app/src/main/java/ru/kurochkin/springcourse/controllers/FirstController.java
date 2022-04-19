0c0a08362c53e99ed1a6f192d9b7cf3e0e7112d1
Andrey Kurochkin <barbedak@gmail.com>
2022-04-19 14:19:05 +0300

    Model. transmit data from controller to view

--- spring-mvc-app/src/main/java/ru/kurochkin/springcourse/controllers/FirstController.java
+++ spring-mvc-app/src/main/java/ru/kurochkin/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.kurochkin.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +28,33 @@ public class FirstController {
     //чтобы не было ошибки используется "required = false", помещающий null в переменную, если параметр не передавался
     @GetMapping("/goodbye")
     public String goodByePage(@RequestParam(value = "name", required = false) String name,
-                              @RequestParam(value = "surname", required = false) String surname){
+                              @RequestParam(value = "surname", required = false) String surname,
+                              Model model){
         System.out.println("Goodbye, " + name + " " + surname);
-
+        model.addAttribute("message","Goodbye, " + name + " " + surname);
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam ("a") int a, @RequestParam("b") int b,
+                                 @RequestParam("action") String action, Model model){
+        double result = 0;
+        if (action.equals("multiplication")){
+            result = a * b;
+        }
+        if (action.equals("addition")){
+            result = a + b;
+        }
+        if (action.equals("subtraction")){
+            result = a - b;
+        }
+        if (action.equals("division") && b!=0){
+            result = a / (double)b;
+        }
+        model.addAttribute("action", action);
+        model.addAttribute("a", a);
+        model.addAttribute("b", b);
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
